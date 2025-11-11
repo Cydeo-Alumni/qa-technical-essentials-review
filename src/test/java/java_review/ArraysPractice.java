31ed1c6d23390cf644fe6e0fbec9188919f851e8
Murodil <murodil@cybertekschool.com>
2025-11-11 12:06:22 -0500

    Java review day02

--- src/test/java/java_review/ArraysPractice.java
+++ src/test/java/java_review/ArraysPractice.java
@@ -1,5 +1,7 @@
 package java_review;
 
+import java.util.Arrays;
+
 public class ArraysPractice {
     public static void main(String[] args) {
         //Declare int array with values 1 to 5
@@ -12,5 +14,40 @@ public class ArraysPractice {
         for (int n : nums) {
             System.out.print(n + " ");
         }
+
+        System.out.println();
+
+        System.out.println(formatArray(nums));
+        System.out.println(formatArray(new int[] {4,2,2,4,6,6}));
+
+        //print array in reverse order
+        int[] arr = {10, 20, 40, 12, 40};
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(reverseArray(nums)));
+        System.out.println(Arrays.toString(reverseArray(new int[] {4,2,2,4,6,6})));
+    }
+
+    //Method formatArray that accepts int[] and returns values separated by space as a String
+    public static String formatArray(int[] nums) {
+        String result = "";
+        for (int i = 0; i < nums.length; i++) {
+            result += nums[i] + " ";
+        }
+        return result.trim();
+    }
+
+    //reverseArray method - accepts int[] and returns reversed int[]
+    public static int[] reverseArray(int[] nums) {
+        //declare empty array with same size
+        int[] reversed = new int[nums.length];
+
+        for (int i = nums.length-1, j = 0; i >= 0; i--, j++) {
+            reversed[j] = nums[i];
+        }
+
+        return reversed;
     }
 }
