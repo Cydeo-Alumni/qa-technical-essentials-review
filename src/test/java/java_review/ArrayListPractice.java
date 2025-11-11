403509e0f35d6622691dce6a8ccc7f3ee7b9364e
Murodil <murodil@cybertekschool.com>
2025-11-11 13:00:34 -0500

    Java review day02

--- src/test/java/java_review/ArrayListPractice.java
+++ src/test/java/java_review/ArrayListPractice.java
@@ -19,9 +19,40 @@ public class ArrayListPractice {
         for (String each : list) {
             System.out.print(each + " ");
         }
+        System.out.println();
+
+        System.out.println(formatArrayList(list));
+        List<String> words = new ArrayList<>(List.of("one", "two", "three", "four"));
+        words.add("five");
+        System.out.println(formatArrayList(words));
+
+        System.out.println(reverseArrayList(words));
+
+        Collections.reverse(list);
+        System.out.println("list = " + list);
     }
 
     /*
     formatArrayList Method accepts arraylist and returns String with values separated by space
      */
+    public static String formatArrayList(List<String> list) {
+        String result = "";
+        for (String each : list) {
+            result += each + " ";
+        }
+        return result.trim();
+    }
+
+   /*
+    reverseArrayList Method accepts List<String> returns List<String>
+    */
+    public static List<String> reverseArrayList(List<String> list) {
+        List<String> reversed = new ArrayList<>();
+
+        for (int i = list.size()-1; i >= 0; i--) {
+            reversed.add(list.get(i));
+        }
+
+        return reversed;
+    }
 }
