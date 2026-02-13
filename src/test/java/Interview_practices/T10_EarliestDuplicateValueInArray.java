c999997b2c5a1e61ac302b0de962d91099ddf8ff
Murodil <murodil@cybertekschool.com>
2026-02-12 19:08:33 -0500

    Java practice day04

--- src/test/java/Interview_practices/T10_EarliestDuplicateValueInArray.java
+++ src/test/java/Interview_practices/T10_EarliestDuplicateValueInArray.java
@@ -6,6 +6,7 @@ public class T10_EarliestDuplicateValueInArray {
     public static void main(String[] args) {
         int[] nums = {2, 1, 3, 7, 3, 2, 0};
         System.out.println(findFirstDup(nums));
+        System.out.println(findFirstDupWithLoop(nums));
     }
     //{2, 1, 3, 7, 3, 2, 0}
     public static int findFirstDup(int[] nums) {
@@ -22,5 +23,23 @@ public class T10_EarliestDuplicateValueInArray {
         }
 
         return -1;
+    }
+
+    public static int findFirstDupWithLoop(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return -1;
+        }
+        int dupIndex = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    if (j < dupIndex) {
+                        dupIndex = j;
+                    }
+                }
+            }
+        }
+
+        return nums[dupIndex];
     }
 }
